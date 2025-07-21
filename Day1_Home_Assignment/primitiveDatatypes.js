ea05ee7e4e80d96d639cf15158e145fd0314a7a6
SwethaArav <swethaaravindh9921@gmail.com>
2025-07-21 12:18:30 +0530

    Day1 Home Assignment
    Day1 Home Assignment

---
+++ Day1_Home_Assignment/primitiveDatatypes.js
@@ -0,0 +1,30 @@
+//Create the following variables using let (not using var) and check their typeOf
+
+let userName='Swetha'
+console.log(userName);
+console.log(typeof userName);
+
+
+let companyName='Accenture'
+console.log(companyName);
+console.log(typeof companyName);
+
+let mobileNumber=9876543210
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+let isAutomation=true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+let hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+let hasTOSCA=null
+console.log(hasTOSCA);
+console.log(typeof hasTOSCA);
+
+const aadharCardNum=998877553423432322544n
+console.log(aadharCardNum);
+console.log(typeof aadharCardNum);
